99d7d73572360dc6580ce2ccb0c1b1528b1c9b4b
Плотников Роман Владимирович <rvplotnikov@edu.hse.ru>
2023-03-07 08:41:24 +0000

    add github api get

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import json
 
 
 CODEWARS_USER_NAME = environ.get('CODEWARS_USER_NAME')
+GITHUB_USER_NAME = environ.get('GITHUB_USER_NAME')
+GITHUB_ACCESS_TOKEN = environ.get('GITHUB_ACCESS_TOKEN')
 
 cw_resp = get("https://www.codewars.com/api/v1/users/{}".format(CODEWARS_USER_NAME))
 with open('data/codewars.json', 'w') as f:
     json.dump(cw_resp.json(), f, indent=4)
+
+gh_resp = get("https://api.github.com/users/{}".format(GITHUB_USER_NAME), headers={
+    'Accept': 'application/vnd.github+json',
+    'Authorization': GITHUB_ACCESS_TOKEN,
+    'X-GitHub-Api-Version': '2022-11-28'
+})
+with open('data/github.json', 'w') as f:
+    json.dump(gh_resp.json(), f, indent=4)
