129c6a346568a6e4db331a978731748241beab53
Плотников Роман Владимирович <rvplotnikov@edu.hse.ru>
2023-03-07 12:50:28 +0000

    more tabs

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ GITHUB_ACCESS_TOKEN = environ.get('GITHUB_ACCESS_TOKEN')
 mkdir("data")
 
 cw_resp = get(f"https://www.codewars.com/api/v1/users/{CODEWARS_USER_NAME}",
-    timeout=10
-)
+        timeout=10)
 with open('data/codewars.json', 'w', encoding="utf-8") as f:
     json.dump(cw_resp.json(), f, indent=4)
 
 gh_resp = get(f"https://api.github.com/users/{GITHUB_USER_NAME}",
-    timeout=10,
-    headers={
-        'Accept': 'application/vnd.github+json',
-        'Authorization': GITHUB_ACCESS_TOKEN,
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
+        timeout=10,
+        headers={
+            'Accept': 'application/vnd.github+json',
+            'Authorization': GITHUB_ACCESS_TOKEN,
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
 )
 with open('data/github.json', 'w', encoding="utf-8") as f:
     json.dump(gh_resp.json(), f, indent=4)
